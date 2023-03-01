043cd2cc183e66a7066487513a181437597e756c
HoJin9622 <kiss0104040@gmail.com>
2023-03-02 00:01:12 +0900

    💎 style: 헤더 스타일링

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,10 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header>
-      <Link href='/'>Hojin&apos;s devlog</Link>
+    <header className='border-b-2 border-b-gray-50 px-3 py-4 font-bold'>
+      <Link href='/' className='cursor-pointer text-red-400 text-xl'>
+        Hojin&apos;s devlog
+      </Link>
     </header>
   )
 }
