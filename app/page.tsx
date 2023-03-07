15059195d0523afff3dc57066d2a25d72b25c62d
HoJin9622 <kiss0104040@gmail.com>
2023-03-08 01:10:44 +0900

    ✨ feat: 깃허브, 메일 주소 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,12 @@
+import Profile from '@/components/Profile'
 import { getCategories } from '@/utils/posts'
-import Image from 'next/image'
-import Link from 'next/link'
 
 export default function Home() {
   const categories = getCategories()
 
   return (
     <div className='px-6'>
-      <div className='py-6'>
-        <div className='flex items-center gap-5'>
-          <Image
-            className='rounded-full w-12 h-12'
-            src='/images/avatar.png'
-            width={48}
-            height={48}
-            alt='avatar'
-          />
-          <div>
-            <div>이호진</div>
-            <div>Software Developer</div>
-          </div>
-        </div>
-      </div>
+      <Profile />
     </div>
     // <nav className='px-3 py-4'>
     //   <h3 className='font-medium text-lg border-b-2 pb-1 mb-3'>카테고리</h3>
