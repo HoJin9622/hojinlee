edfba7237dae700a7200050041a5ff9b88fc41ee
HoJin9622 <kiss0104040@gmail.com>
2023-03-08 00:42:17 +0900

    💎 style: 헤더 디자인 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header className='border-b-2 border-b-gray-50 px-3 py-4 font-bold'>
-      <Link href='/' className='cursor-pointer text-red-400 text-xl'>
+    <header className='flex border-b-2 border-b-gray-50 px-6 h-14 font-bold items-center'>
+      <Link href='/' className='cursor-pointer text-xl'>
         Hojin&apos;s devlog
       </Link>
     </header>
