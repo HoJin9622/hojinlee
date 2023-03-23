60ca8507a401cd174b69843072d4a43bec763945
HoJin9622 <kiss0104040@gmail.com>
2023-03-24 01:19:12 +0900

    🏗️ build: package를 찾지 못하는 문제 해결

--- .pnp.cjs
+++ .pnp.cjs
@@ -33,6 +33,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         [null, {\
           "packageLocation": "./",\
           "packageDependencies": [\
+            ["typescript", "patch:typescript@npm%3A4.9.5#~builtin<compat/typescript>::version=4.9.5&hash=23ec76"]\
           ],\
           "linkType": "SOFT"\
         }]\
@@ -3328,7 +3329,8 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         ["workspace:.", {\
           "packageLocation": "./",\
           "packageDependencies": [\
-            ["nextlevels", "workspace:."]\
+            ["nextlevels", "workspace:."],\
+            ["typescript", "patch:typescript@npm%3A4.9.5#~builtin<compat/typescript>::version=4.9.5&hash=23ec76"]\
           ],\
           "linkType": "SOFT"\
         }]\
--- packages/devlog/app/layout.tsx
+++ packages/devlog/app/layout.tsx
@@ -1,19 +1,19 @@
-import Header from '@/components/Header'
-import Script from 'next/script'
-import './globals.css'
+import Header from "@/components/Header";
+import Script from "next/script";
+import "./globals.css";
 
 export const metadata = {
-  title: '이호진의 기술블로그',
-  description: '개발 내용을 정리한 블로그입니다.',
-}
+  title: "이호진의 기술블로그",
+  description: "개발 내용을 정리한 블로그입니다.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='ko'>
+    <html lang="ko">
       <body>
         <Header />
         <main>{children}</main>
@@ -21,9 +21,9 @@ export default function RootLayout({
 
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG}`}
-        strategy='afterInteractive'
+        strategy="afterInteractive"
       />
-      <Script id='google-analytics' strategy='afterInteractive'>
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
@@ -33,5 +33,5 @@ export default function RootLayout({
         `}
       </Script>
     </html>
-  )
+  );
 }
