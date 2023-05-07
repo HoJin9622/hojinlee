9845e9a97957644cd3e8a44d10ffb439737fedb9
HoJin9622 <kiss0104040@gmail.com>
2023-05-07 23:08:55 +0900

    ✨ feat: 카테고리 디자인 변경

--- components/Category.tsx
+++ components/Category.tsx
@@ -12,8 +12,10 @@ export default function Category({ category, path, count, active }: Props) {
   return (
     <Link
       href={path}
-      className={`px-2 py-1 border-2 rounded-2xl text-sm ${
-        active && 'bg-blue-200'
+      className={`px-2 py-1 rounded-2xl text-sm ${
+        active
+          ? 'text-white bg-gray-600'
+          : 'text-gray-400 bg-gray-100 hover:bg-gray-200'
       }`}
       key={category}
     >
