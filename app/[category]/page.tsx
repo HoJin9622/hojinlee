cf63d4bb942ce57e06ceb8612ea255f340099664
이호진 <kiss0104040@gmail.com>
2023-05-09 00:33:07 +0900

    ✨ feat: 카테고리 페이지 layout 생성 (#8)

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import Categories from '@/components/Categories';
 import Post from '@/components/Post';
-import Profile from '@/components/Profile';
 import { getCategories, getCategoryPostMetadata } from '@/utils/posts';
 
 type Props = {
@@ -13,24 +11,18 @@ export default function Category({ params: { category } }: Props) {
   const posts = getCategoryPostMetadata(category);
 
   return (
-    <div className="px-6">
-      <Profile />
-
-      <Categories path={category} />
-
-      <article className="space-y-1 divide-y">
-        {posts.map((post) => (
-          <Post
-            key={post.slug}
-            category={post.category}
-            date={post.date}
-            slug={post.slug}
-            title={post.title}
-            coverImage={post.coverImage}
-          />
-        ))}
-      </article>
-    </div>
+    <article className="space-y-1 divide-y">
+      {posts.map((post) => (
+        <Post
+          key={post.slug}
+          category={post.category}
+          date={post.date}
+          slug={post.slug}
+          title={post.title}
+          coverImage={post.coverImage}
+        />
+      ))}
+    </article>
   );
 }
 
