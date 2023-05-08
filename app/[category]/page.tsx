8a8cf87cccd95f74b681b42bccd92c4770d94c85
이호진 <kiss0104040@gmail.com>
2023-05-09 00:56:02 +0900

    refactor: 네이밍 변경 및 PostList Component 생성 (#10)
    * ♻️ refactor: 게시물 목록 component 분리
    
    * ♻️ refactor: comments -> CommentList로 네이밍 변경
    
    * ♻️ refactor: categories -> CategoryList로 네이밍 변경

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import Post from '@/components/Post';
-import categoryUrls from '@/constants/category';
+import PostList from '@/components/PostList';
 import { getCategories, getCategoryPostMetadata } from '@/utils/posts';
 
 type Props = {
@@ -11,20 +10,7 @@ type Props = {
 export default function Category({ params: { category } }: Props) {
   const posts = getCategoryPostMetadata(category);
 
-  return (
-    <article className="space-y-1 divide-y">
-      {posts.map((post) => (
-        <Post
-          key={post.slug}
-          category={categoryUrls[post.category]}
-          date={post.date}
-          slug={post.slug}
-          title={post.title}
-          coverImage={post.coverImage}
-        />
-      ))}
-    </article>
-  );
+  return <PostList posts={posts} />;
 }
 
 export const generateStaticParams = async () => {
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
-import Categories from '@/components/Categories';
-import Post from '@/components/Post';
+import CategoryList from '@/components/CategoryList';
+import PostList from '@/components/PostList';
 import Profile from '@/components/Profile';
-import categoryUrls from '@/constants/category';
 import { getPostMetadata } from '@/utils/posts';
 
 export default function Home() {
@@ -13,20 +12,9 @@ export default function Home() {
     <div className="px-6">
       <Profile />
 
-      <Categories path="/" />
+      <CategoryList path="/" />
 
-      <article className="space-y-1 divide-y">
-        {posts.map((post) => (
-          <Post
-            key={post.slug}
-            category={categoryUrls[post.category]}
-            date={post.date}
-            slug={post.slug}
-            title={post.title}
-            coverImage={post.coverImage}
-          />
-        ))}
-      </article>
+      <PostList posts={posts} />
     </div>
   );
 }
