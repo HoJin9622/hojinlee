6e010c44a5b3105cae334ddb096638e073f8389d
HoJin9622 <kiss0104040@gmail.com>
2023-05-27 22:19:30 +0900

    ♻️ refactor: 함수 분리 및 타입 파일 생성

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,36 +1,14 @@
 import { GetServerSideProps } from 'next';
 
-import { getPostMetadata, PostMetadata } from '@/utils/posts';
-
-const EXTERNAL_DATA_URL = 'https://devlog.nextlevels.net';
-
-function generateSiteMap(posts: PostMetadata[]) {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>${EXTERNAL_DATA_URL}</loc>
-        </url>
-      ${posts
-        .map(
-          ({ slug, date }) => `
-        <url>
-            <loc>${`${EXTERNAL_DATA_URL}/posts/${slug}`}</loc>
-            <lastmod>${date}</lastmod>
-            <priority>1.0</priority>
-        </url>
-      `,
-        )
-        .join('')}
-    </urlset>
-  `;
-}
+import generateSitemap from '@/utils/generateSitemap';
+import { getPostMetadata } from '@/utils/posts';
 
 function SiteMap() {}
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const posts = getPostMetadata();
 
-  const sitemap = generateSiteMap(posts);
+  const sitemap = generateSitemap(posts);
 
   res.setHeader('Content-Type', 'text/xml');
 
---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+type Post = {
+  title: string;
+  date: string;
+  subtitle: string;
+  category: string;
+  slug: string;
+  coverImage?: string;
+};
