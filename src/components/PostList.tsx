2a9e4d7a6255095a8594055b314822a908aa4f6d
이호진 <kiss0104040@gmail.com>
2023-05-27 22:22:03 +0900

    ♻️ refactor: 함수 분리 및 타입 파일 생성 (#31)

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 import categoryUrls from '@/constants/category';
-import { PostMetadata } from '@/utils/posts';
 
 import Post from './Post';
 
 type Props = {
-  posts: PostMetadata[];
+  posts: Post[];
 };
 
 export default function PostList({ posts }: Props) {
