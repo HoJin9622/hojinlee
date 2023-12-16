f862ad2a100008f57780e84eeb13880303784c17
HoJin9622 <kiss0104040@gmail.com>
2023-12-17 01:09:58 +0900

    feat: 게시물 목록 표시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { getPosts } from '@/utils/post'
+
 export default function Home() {
+  const posts = getPosts()
   return (
     <main>
       <h3>Latest Posts</h3>
+      {posts.map((post) => (
+        <Link href={`/posts/${post.slug}`} key={post.slug}>
+          <Image src={post.coverImage} width={150} height={150} alt='' />
+          <h4>{post.title}</h4>
+          <div>{post.subtitle}</div>
+        </Link>
+      ))}
     </main>
   )
 }
---
+++ src/utils/post.ts
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+import path from 'path'
+
+const FOLDER = path.join(process.cwd(), 'src/posts')
+
+const getAllFileNames = () => {
+  const names = fs.readdirSync(FOLDER)
+  return names
+}
+
+const getFileMatterResult = (fileName: string) => {
+  const content = fs.readFileSync(`${FOLDER}/${fileName}`)
+  const matterResult = matter(content)
+  return matterResult
+}
+
+export const getPosts = () => {
+  const names = getAllFileNames()
+  const posts = names.map((name) => {
+    const {
+      data: { title, subtitle, date, category, coverImage, draft },
+    } = getFileMatterResult(name)
+    return {
+      title,
+      subtitle,
+      date,
+      category,
+      coverImage,
+      draft,
+      slug: name.replace('.mdx', ''),
+    }
+  })
+  return posts
+}
