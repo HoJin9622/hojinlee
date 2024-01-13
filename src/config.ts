43b280ef8b97c97060443df9779434d1f7be6a50
HoJin9622 <kiss0104040@gmail.com>
2024-01-13 20:21:05 +0900

    feat: author, title 설정

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  author: "Jin",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Jin - Web Frontend Developer",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 8,
